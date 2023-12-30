7d03bc7452dc49b487d4da894951335cc8b41e45
RaspingHare3640 <60298600+RaspingHare3640@users.noreply.github.com>
2023-12-30 13:40:39 -0800

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button @btn-click="$emit('toggle-add-task')"  
+        :text="showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? 'red' : 'green' " />
+        <!-- <Button text="Update Task" color="blue" />
+        <Button text="Delete Task" color="red" /> -->
+        <!-- <Time/> -->
+    </header>
+</template>
+
+<script>
+import Button from "./Button"
+import Time from "./Time"
+
+export default ({
+   name:"Header",
+   props: {
+       title: String,
+       showAddTask: Boolean
+   },
+   components: {
+       Button,
+       Time
+   }
+})
+
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items:  center;
+        margin-bottom: 20px;
+    }
+</style>
